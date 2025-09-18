f182392bde3c835d84fea3ff56e0fc992b210948
Ben <bendanderson13@gmail.com>
2025-09-17 22:33:50 -0500

    initial commit

---
+++ src/main/java/com/example/spectrum_webapi/SpectrumWebapiApplication.java
@@ -0,0 +1,13 @@
+package com.example.spectrum_webapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpectrumWebapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpectrumWebapiApplication.class, args);
+	}
+
+}
